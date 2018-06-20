e7206548832b8d53601396228e05f74cd7e46f98
Joanna Buchała <joanna.b2@op.pl>
2018-06-20 22:31:42 +0200

    20_4

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import DevTools from './devtools';
+import { Router, hashHistory } from 'react-router';
 import {getCountries} from './actions/actions-countries';
+import routes from './routes';
 
 render(
 	<Provider store={store}>
 		<div>
-			<h1>Project init</h1>
+			<Router history={hashHistory} routes={routes}/>
 			<DevTools/>
 		</div>
 	</Provider>,
