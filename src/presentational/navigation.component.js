c8c1a59cb31329c97ba7a1745125491504627ad3
Joanna Buchała <joanna.b2@op.pl>
2018-06-21 23:13:18 +0200

    finished implementation, bugs to fix

--- src/presentational/navigation.component.js
+++ src/presentational/navigation.component.js
@@ -10,7 +10,7 @@ const Navigation = props => (
                 <div className="navbar-header">
                     <Link className="navbar-brand" to="/">Countries.js</Link>
                 </div>
-                <div className="collapse navbar-collapse">
+                <div>
                     <ul className="nav navbar-nav">
                         <li><Link to="/countries">Countries</Link></li>
                         <li><Link to="/continents">Continents</Link></li>
